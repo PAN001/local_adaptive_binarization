5da1c468c51063027ccdc7fe8671dece062e6c64
PAN001 <pan.atopos@gmail.com>
2019-04-29 10:47:47 -0400

    output

---
+++ CMakeFiles/binarizewolfjolion_sauvola.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cuda_compile.dir/./cuda_compile_generated_binarizewolfjolion_sauvola.cu.o"
+  "CMakeFiles/binarizewolfjolion_sauvola.dir/main.cpp.o"
+  "CMakeFiles/binarizewolfjolion_sauvola.dir/timing.cpp.o"
+  "binarizewolfjolion_sauvola.pdb"
+  "binarizewolfjolion_sauvola"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/binarizewolfjolion_sauvola.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ serial/CMakeFiles/binarizewolfjolion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/binarizewolfjolion.dir/binarizewolfjolion.cpp.o"
+  "CMakeFiles/binarizewolfjolion.dir/timing.cpp.o"
+  "binarizewolfjolion.pdb"
+  "binarizewolfjolion"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/binarizewolfjolion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
