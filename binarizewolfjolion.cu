c264f1aa5e29a0368739484dcfac6acfecc68df0
PAN001 <pan.atopos@gmail.com>
2019-05-03 18:14:55 -0400

    test

--- binarizewolfjolion.cu
+++ binarizewolfjolion.cu
@@ -226,7 +226,7 @@ void NiblackSauvolaWolfJolionWrapper(Mat input, Mat output, int winx, int winy, 
     getTimeMonotonic(&startTime);
 
     Mat im_sum, im_sum_sq;
-    integral(input, im_sum, im_sum_sq, CV_64F);
+    cv::gpu::integral(input, im_sum, im_sum_sq, CV_64F);
 
     timespec integralEndTime;
     getTimeMonotonic(&integralEndTime);
@@ -236,7 +236,7 @@ void NiblackSauvolaWolfJolionWrapper(Mat input, Mat output, int winx, int winy, 
     timespec minMaxLocStartTime;
     getTimeMonotonic(&minMaxLocStartTime);
     double min_I, max_I;
-    minMaxLoc(input, &min_I, &max_I);
+    cv::gpu::minMaxLoc(input, &min_I, &max_I);
     timespec minMaxLocEndTime;
     getTimeMonotonic(&minMaxLocEndTime);
     cout << "  --cv::minMaxLoc Time: " << diffclock(minMaxLocStartTime, minMaxLocEndTime) << "ms." << endl;
